aa9235db073f5bcb44a3fda1e52c705777543c2c
marsmallowz <alsandymaulana15@email.com>
2022-10-28 11:09:20 +0700

    Perubahan import Date index.js

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,9 @@
-import Head from "next/head";
-import Layout, { siteTitle } from "../components/layout";
-import utilStyles from "../styles/utils.module.css";
-import { getSortedPostsData } from '../lib/posts';
-import Link from 'next/link';
-import Date from '../../components/date';
-
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData();
-  return {
-    props: {
-      allPostsData,
-    },
-  };
-}
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+import { getSortedPostsData } from '../lib/posts'
+import Link from 'next/link'
+import Date from '../components/date'
 
 export default function Home({ allPostsData }) {
   return (
@@ -21,7 +12,11 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Halo Guys</p>
+        <p>I Love Flutter</p>
+        <p>
+          (This is a sample website - you’ll be building a site like this in{' '}
+          href="https://nextjs.org/learn" our Next.js tutorial.)
+        </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
@@ -41,4 +36,13 @@ export default function Home({ allPostsData }) {
       </section>
     </Layout>
   )
+}
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData()
+  return {
+    props: {
+      allPostsData
+    }
+  }
 }
